ec18a10d7de6f67f1fe58a6fc8e7ec0ff8b2a978
anki2001ta <ankitadeb1202@gmail.com>
2022-11-10 23:34:04 +0530

    foot

---
+++ medieasy/src/Pages/place.jsx
@@ -0,0 +1,31 @@
+import React from "react"
+import {Input} from "@chakra-ui/react"
+const {useState, useEffect} = React;
+
+const placeholderText = ["Medicine", "Shampoo", "Health Drink"];
+
+const Test = () => {
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const timer = () => {
+      setIndex(prevIndex => {
+        if(prevIndex === placeholderText.length - 1){
+          return 0;
+        } 
+        return prevIndex + 1;
+      })
+    };
+    setInterval(timer, 2000);
+    
+    //cleanup function in order clear the interval timer
+    //when the component unmounts
+    return () => { clearInterval(timer); }
+  }, []);
+
+  return (
+    <Input  style={{textIndent:"25px"}}type='Search' placeholder={"Search For"+" "+placeholderText[index]}  w={"600px"} position={"relative"} pr={"160px"}/>
+  )
+}
+export default Test
+// <Input type='tel' placeholder={"Search For"+" "+placeholderText[index]}  w={"580px"} position={"relative"} pr={"160px"}/>
